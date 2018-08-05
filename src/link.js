e89c0447757cff8a22abdbb0352fa13fb392413e
Thanh Doan <tmdoan98@gmail.com>
2018-08-05 22:43:30 +0100

    remove adding event listener from link

--- src/link.js
+++ src/link.js
@@ -2,10 +2,7 @@ const Link = (props, path, linkName) => {
     let link = document.createElement('a');
     link.href = path;
     link.innerHTML = linkName;
-    link.addEventListener('click', () => {
-        event.preventDefault();
-        props.router.setCurrentPath(path);
-    });
+
     return link;
 }
 
